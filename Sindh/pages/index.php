3de640babb29030df2507af5f6f7f32d8770a5ed
Saqib <hafizsaqi555@gmail.com>
2025-12-24 10:37:19 +0500

    remove ignored files from tracking

---
+++ Sindh/pages/index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-------- META TAGS  -------->
+        <meta name="google-adsense-account" content="ca-pub-4828179335177828">
+    <meta name="description"
+        content="03003006220 | 03453406220 | The Army Dog Center is a premier facility dedicated to the training, care, and rehabilitation of military working dogs. Our team consists of experienced trainers, veterinarians, and animal behaviorists who understand the unique needs of these courageous animals.">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="https://services.armydogcenter.org.pk/">
+
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="Army Dog Center Pakistan | 03003006220 | 03453406220">
+    <meta property="og:description"
+        content="03003006220 | 03453406220 | The Army Dog Center is a premier facility dedicated to the training, care, and rehabilitation of military working dogs. Our team consists of experienced trainers, veterinarians, and animal behaviorists who understand the unique needs of these courageous animals.">
+    <meta property="og:url" content="https://services.armydogcenter.org.pk/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://armydogcenter.org.pk/images/dogimg-14.jpeg">
+    <title>Army Dog Center | Services</title>
+       <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Army Dog Center Pakistan | 03003006220 | 03453406220">
+    <meta name="twitter:description" content="        
+             چوری، ڈکیتی، قتل اور اغوا جیسے جرائم کے خلاف فوری مدد کے لیے ہمہ وقت دستیاب۔ ہمارے ماہر کتے جرائم کے نشانات ڈھونڈنے اور اہم شواہد فراہم کرنے میں مددگار ثابت ہوتے ہیں۔ ہم 24/7 ہر جگہ خدمات فراہم کرتے ہیں تاکہ آپ کو مکمل تحفظ اور اطمینان حاصل ہو۔
+             
+            ">
+    <meta name="twitter:image" content="https://armydogcenter.org.pk/images/dogimg-14.jpeg">
+    <!-- Favicon link -->
+    <link rel="icon" href="https://armydogcenter.org.pk/images/logo.webp" type="image/x-icon">
+    <link rel="shortcut icon" href="https://armydogcenter.org.pk/images/logo.webp" type="image/x-icon">
+    <!------- JSON - LD ---------->
+    <script type="application/ld+json">
+            {
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Army Dog Center Pakistan | 03003006220 | 03453406220",
+              "url": "https://services.armydogcenter.org.pk/",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://services.armydogcenter.org.pk/search?query={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            }
+            </script>
+
+
+</head>
+<body>
+      <?php include '../includes/header.php' ?>
+    <!------- SERVICES SECTION --------->
+    <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-8 md:flex-row flex-col items-center">
+          <div
+            class="lg:flex-grow md:w-1/2 flex flex-col md:items-start items-center text-left">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-teal-600">Our Services
+            </h1>
+            <p class="mb-4 leading-relaxed text-gray-600 text-lg">At the Army Dog Center, we offer a comprehensive range of services tailored to meet the unique needs of military working dogs. Our expert training programs focus on obedience, agility, and specialized skills essential for military operations. We provide top-notch veterinary care, including routine check-ups and emergency services, ensuring the health and well-being of every dog. Additionally, our rehabilitation services support dogs recovering from injuries or trauma, helping them regain their strength and confidence. We are dedicated to enhancing the quality of life for military dogs and their handlers through our specialized offerings.</p>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 lg:pl-24 md:pl-16  w-5/6 mb-10 md:mb-0">
+            <img class="object-cover object-center rounded" alt="hero" src="https://armydogcenter.org.pk/images/dogcard-1.webp">
+          </div>
+        </div>
+      </section>
+      <!------- FEATURED SECTION --------->
+      <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-8 md:flex-row flex-col-reverse items-center">
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <img class="object-cover object-center rounded" alt="hero" src="https://armydogcenter.org.pk//images/dogcard-4.webp">
+          </div>
+          <div
+            class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-left">
+            <p class="mb-4 leading-relaxed text-gray-600 text-lg">We highlight the inspiring stories of military dogs who have successfully transitioned to civilian life. These dogs, once heroes on the battlefield, now serve in various roles, including therapy and search and rescue. Our program showcases their adaptability and the vital skills they bring to their new environments. Through specialized training and rehabilitation, we prepare these dogs for their next chapter, ensuring they continue to make a difference in the lives of those around them. Their journeys exemplify resilience and the enduring bond between dogs and humans.</p>
+          </div>
+        </div>
+      </section>
+      <!------- CITIES SECTION --------->
+      <section class="px-5 py-8">
+      <div
+      class=" flex flex-col items-center text-left">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-teal-500">Cities We Serve
+      </h1>
+      <p class="mb-4 leading-relaxed text-gray-600 text-lg text-center">Our services are available in various provinces of Pakistan. We are serving 24/7 in major cities of Pakistan</p>
+      </div>
+      <div class=" grid gap-4" style="grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));">
+        <details class="mx-4 group [&_summary::-webkit-details-marker]:hidden" open>
+          <summary
+            class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900">
+            <h2 class="font-bold text-xl text-teal-600 ">Army Dog Center Sindh</h2>
+  
+            <svg class="size-5 shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Hyderabad</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Karachi</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Hub Chowki</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Thatta</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Mithi</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Badin</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Sukkur</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Mirpur Khas</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Umar Kot</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Shikarpur</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Nawabshah</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Moro</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Dadu</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Jacobabad</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Larkana</li>
+                        <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Sanghar</li>
+             </details>
+       
+             <details class="mx-4 group [&_summary::-webkit-details-marker]:hidden" open>
+          <summary
+            class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900">
+            <h2 class="font-bold text-xl text-teal-600 ">Army Dog Center Balochistan</h2>
+  
+            <svg class="size-5 shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+  
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Quetta</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Turbat</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Khuzdar</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Gwadar </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center kalat</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center lora lai</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Zhob</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Chaman</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Dalbandin</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Panjgur</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Kohlu</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Mussa khel</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Barkhan</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Dera bugti </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Ziarat </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Qila abdullah </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Taftan</li>
+        </details>
+             <details class="mx-4 group [&_summary::-webkit-details-marker]:hidden" open>
+          <summary
+            class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900">
+            <h2 class="font-bold text-xl text-teal-600 ">Army Dog Center Punjab</h2>
+  
+            <svg class="size-5 shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+  
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Lahore</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Islamabad</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Rawalpindi</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Multan</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Faisalabad</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Sialkot</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Gujranwala</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Bahawalpur</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Sargodha</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Sheikhupura</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Jhelum</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Kasur</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center D G Khan</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Bahawalnagar</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Attock</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Mianwali</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4">Army Dog Center Khanewal</li>
+          
+        </details>
+        <details class="mx-4 group [&_summary::-webkit-details-marker]:hidden" open>
+          <summary
+            class="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900">
+            <h2 class="font-bold text-xl text-teal-600 ">Army Dog Center KPK</h2>
+  
+            <svg class="size-5 shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+  
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Charsadda</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Gilgit</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Haripur</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Peshawar</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Chitral</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army dog Center Abbottabad</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Kohat</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Bannu </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center D I Khan</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Mardan</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Mansehra </li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center buner</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center swabi</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center Nowshera</li>
+          <li class="text-gray-500 list-disc font-semibold my-3 ml-4"> Army Dog Center mingora swat </li>
+        </details>
+       
+        
+       
+      
+      </div>
+    </section>
+      <!------- STATISTICS SECTION --------->
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-8 mx-auto">
+          <div class="flex flex-wrap -m-4 text-left">
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg text-center">
+                <h2 class="title-font font-medium text-3xl text-gray-900">100+</h2>
+                <p class="leading-relaxed text-teal-600 font-semibold">PROJECTS DONE</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg text-center">
+                <h2 class="title-font font-medium text-3xl text-gray-900">300+</h2>
+                <p class="leading-relaxed text-teal-600 font-semibold">HANDLING PROJECTS</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg text-center">
+                <h2 class="title-font font-medium text-3xl text-gray-900">7</h2>
+                <p class="leading-relaxed text-teal-600 font-semibold">ANNUAL AWARD</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-200 px-4 py-6 rounded-lg text-center">
+                <h2 class="title-font font-medium text-3xl text-gray-900">100%</h2>
+                <p class="leading-relaxed text-teal-600 font-semibold">CLIENT SATISFACTION</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+        <?php include '../includes/footer.php' ?>
+      <script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
